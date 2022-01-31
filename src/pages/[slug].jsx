2ba210e75800883c5cb7ee1075defe60945eae27
LuannMateus <luanmateuslima34@gmail.com>
2022-01-31 11:32:46 -0300

    Add getStaticPaths with fallback true

--- src/pages/[slug].jsx
+++ src/pages/[slug].jsx
@@ -1,27 +1,34 @@
 import P from 'prop-types';
 import { loadPages } from '../api/loadPages';
 import { Home } from '../templates/Home';
-
+import { useRouter } from 'next/router';
+import { Loading } from '../templates/Loading';
 export default function Page({ data }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loading />;
+  }
+
   return <Home data={data} />;
 }
 
-// export const getStaticPaths = async () => {
-//   const paths = (await loadPages()).map((page) => {
-//     return {
-//       params: {
-//         slug: page.slug,
-//       },
-//     };
-//   });
+export const getStaticPaths = async () => {
+  // const paths = (await loadPages()).map((page) => {
+  //   return {
+  //     params: {
+  //       slug: page.slug,
+  //     },
+  //   };
+  // });
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
 
-export const getServerSideProps = async (ctx) => {
+export const getStaticProps = async (ctx) => {
   let data = null;
 
   try {
@@ -30,7 +37,7 @@ export const getServerSideProps = async (ctx) => {
     data = null;
   }
 
-  if (!data) {
+  if (!data || !data.length) {
     return {
       notFound: true,
     };
